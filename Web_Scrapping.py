a8c93fbbecb307b65f7eeb4402aa50d44feb6bc8
Sameer Khowaja <sameerkhowaja12@gmail.com>
2020-04-10 01:24:23 +0500

    Scrapping Data

--- Web_Scrapping.py
+++ Web_Scrapping.py
@@ -1,37 +1,63 @@
 import requests
-from bs4 import BeautifulSoup
-from csv import writer
+import bs4
+import csv
 
-response = requests.get('https://www.kiryano.com/home.php?pid=65')
-soup = BeautifulSoup(response.text, 'html.parser')
+response = requests.get('http://yayvo.com/')
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+# print(soup)
 
-mainRoot = soup.find(class_="shop-page-container mb-50").find(class_="container").find(class_="row").find(class_="col-lg-9 order-1 order-lg-2 mb-sm-35 mb-xs-35")
-posts = mainRoot.find(class_='shop-product-wrap grid row no-gutters mb-35').findAll(class_='col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6')
+mainRoot = soup.find(class_="ma-tabsproduct-contain").find(class_="ma-tabs-content")
+posts = mainRoot.findAll(class_="flexslider carousel")
+# print(posts)
 
-with open('ProductScrapped.csv', 'w') as csv_file:
-    csv_writer = writer(csv_file)
+Product_List = []
+# Working on HTML Text
+print(['Product Category', 'Link', 'Name', 'New Price', 'Old Price', 'Discount', 'Image Source'])
+for post in posts:
+    product_Class = post.findAll(class_="newproductslider-item")
+    # print(product_Class)
+    for products in product_Class:
+        # Product Link
+        Product_Link = products.find(class_="product-image rel-pos")['href']
+        # Product Discount
+        try:
+            Product_Discount = products.find(class_="discount_Span").get_text().replace("-", "")
+        except AttributeError:
+            Product_Discount = "None"
+        # Product Image
+        Product_Image = products.find(class_="b-lazy")['data-src']
+        # print(Product_Link, Product_Discount, Product_Image)
+        # Product New Price
+        try:
+            NewPrice = products.find(class_="custom_pricespec").find(class_="price").get_text().replace("  ", "")
+        except AttributeError:
+            NewPrice = products.find(class_="custom_pricereg").find(class_="price").get_text().replace("  ", "")
+        # Product Old Price
+        try:
+            OldPrice = products.find(class_="custom_pricedisc").find(class_="price").get_text().replace("  ", "")
+        except AttributeError:
+            OldPrice = "None"
+        # print(NewPrice, OldPrice)
+        # Product Category
+        Product_Category = products.find(class_="cstm_brnd").find("span").get_text()
+        # Product Name
+        Product_Name = products.find(class_="custom_prodname")["title"]
+        # print(Product_Category, Product_Name)
+
+        Product_List.append([str(Product_Category), str(Product_Link), str(Product_Name), str(NewPrice), str(OldPrice), str(Product_Discount), str(Product_Image)])
+        # print([str(Product_Category), str(Product_Link), str(Product_Name), str(NewPrice), str(OldPrice), str(Product_Discount), str(Product_Image)])
+        # csv_writer.writerow(Product_List)
+
+print("Total Products:", len(Product_List))
+
+
+# CSV File
+with open('Scrapped_Data.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
     headers = ['Product Category', 'Product Link', 'Name', 'New Price', 'Old Price', 'Discount', 'Image Source']
-    csv_writer.writerow(headers)
-
-    for post in posts:
-        findProductLink = post.find(class_="gf-product shop-grid-view-product").find(class_="image").find('a')['href']
-        findProductDiscount = post.find(class_="gf-product shop-grid-view-product").find(class_="image").find(class_="onsale").get_text().replace(' Off', '')
-        findImageSource = post.find(class_="gf-product shop-grid-view-product").find(class_="image").find('img')['src']
-        print(findProductLink)
-
-        findProductContent = post.find(class_="gf-product shop-grid-view-product").find(class_="product-content")
-        findProductTitle = findProductContent.find(class_="product-title").find('a').get_text()
-        findProductNewPrice = findProductContent.find(class_="price-box").find(class_="discounted-price").get_text()
-        findProductOldPrice = findProductContent.find(class_="price-box").find(class_="main-price").get_text()
-
-        findProductCategory = findProductContent.find(class_="product-categories")
-        # If Category count increase then manual work require
-        categoryList = findProductCategory.findAll('a')
-        category1 = categoryList[0].get_text()
-        category2 = categoryList[1].get_text()
-        ProductCategory = category1 + ", " + category2
-
-        print(ProductCategory, findProductLink, findProductTitle, findProductNewPrice, findProductOldPrice, findProductDiscount, findImageSource)
-        csv_writer.writerow([ProductCategory, findProductLink, findProductTitle, findProductNewPrice, findProductOldPrice, findProductDiscount, findImageSource])
+    writer.writerow(headers)
+    for products in range(len(Product_List)):
+        print(Product_List[products])
+        writer.writerow(Product_List[products])
 
 # End of Program
